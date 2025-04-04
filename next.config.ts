05c7c6c8ce3347ce40b070955654682ce276f084
Metheavy <meathvy@gmail.com>
2025-04-04 14:42:27 +0700

    Deploy to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: 'export', // Ensures static export
+  basePath: "/portfolio.github.io", // Change to your repo name
+  images: {
+    unoptimized: true, // Since GitHub Pages doesn’t support Next.js image optimization
+  },
 };
 
 export default nextConfig;
